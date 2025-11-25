02168e80d2d78b12cd265daaf3b294e94d77e7f0
leonel <viefmoon@gmail.com>
2025-11-25 08:00:53 -0600

    a

--- build/CMakeFiles/git-data/grabRef.cmake
+++ build/CMakeFiles/git-data/grabRef.cmake
@@ -18,7 +18,7 @@ set(HEAD_HASH)
 file(READ "C:/Users/Leone/OneDrive/Desktop/simple_video_server/build/CMakeFiles/git-data/HEAD" HEAD_CONTENTS LIMIT 1024)
 
 string(STRIP "${HEAD_CONTENTS}" HEAD_CONTENTS)
-set(GIT_DIR "C:/Users/Leone/esp/v5.5.1/esp-idf/.git")
+set(GIT_DIR "C:/Users/Leone/OneDrive/Desktop/simple_video_server/.git")
 # handle git-worktree
 if(EXISTS "${GIT_DIR}/commondir")
 	file(READ "${GIT_DIR}/commondir" GIT_DIR_NEW LIMIT 1024)
